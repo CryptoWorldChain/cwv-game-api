c327822ec69a00a1e3bea312cc0842bd0554af28
wumin <bulenessdragon@sina.com>
2018-03-30 23:35:52 +0700

    common增加信息

--- common/src/main/java/org/brewchain/cwv/common/service/CountryListService.java
+++ common/src/main/java/org/brewchain/cwv/common/service/CountryListService.java
@@ -54,31 +54,49 @@ public class CountryListService extends SessionModules<PBCountryList> {
 //			example.createCriteria().and
 //			sysDaos.cwvgamecountryDao.selectByExample(example);
 			Countries.Builder countries = Countries.newBuilder();
-			countries.setDomainCode("AL");//国际域名缩写
-			countries.setReginCode("313");//地区代码
-			countries.setShortName("Albania");//简称
-			countries.setFullName("Republic of Albania");//全称
-			countries.setDescEn("Republic of Albania");//英文描述
-			countries.setPhoneCode("355");//电话代码
+			countries.setDomainCode("AD");//国际域名缩写
+			countries.setReginCode("20");//地区代码
+			countries.setShortName("Andorra");//简称
+			countries.setFullName("Andorra");//全称
+			countries.setDescEn("Andorra");//英文描述
+			countries.setPhoneCode("376");//电话代码
 			ret.addCountries(countries);
 			
 			Countries.Builder countries1 = Countries.newBuilder();
-			countries1.setDomainCode("AL");//国际域名缩写
-			countries1.setReginCode("313");//地区代码
-			countries1.setShortName("Albania");//简称
-			countries1.setFullName("Republic of Albania");//全称
-			countries1.setDescEn("Republic of Albania");//英文描述
-			countries1.setPhoneCode("355");//电话代码
+			countries1.setDomainCode("AU");//国际域名缩写
+			countries1.setReginCode("36");//地区代码
+			countries1.setShortName("Australia");//简称
+			countries1.setFullName("Australia");//全称
+			countries1.setDescEn("Australia");//英文描述
+			countries1.setPhoneCode("61");//电话代码
 			ret.addCountries(countries1);
 			
 			Countries.Builder countries2 = Countries.newBuilder();
 			countries2.setDomainCode("AL");//国际域名缩写
 			countries2.setReginCode("313");//地区代码
 			countries2.setShortName("Albania");//简称
-			countries2.setFullName("Republic of Albania");//全称
-			countries2.setDescEn("Republic of Albania");//英文描述
+			countries2.setFullName("Albania");//全称
+			countries2.setDescEn("Albania");//英文描述
 			countries2.setPhoneCode("355");//电话代码
 			ret.addCountries(countries2);
+			
+			Countries.Builder countries3 = Countries.newBuilder();
+			countries3.setDomainCode("AT");//国际域名缩写
+			countries3.setReginCode("40");//地区代码
+			countries3.setShortName("Austria");//简称
+			countries3.setFullName("Austria");//全称
+			countries3.setDescEn("Austria");//英文描述
+			countries3.setPhoneCode("43");//电话代码
+			ret.addCountries(countries3);
+			
+			Countries.Builder countries4 = Countries.newBuilder();
+			countries4.setDomainCode("AZ");//国际域名缩写
+			countries4.setReginCode("31");//地区代码
+			countries4.setShortName("Azerbaijan");//简称
+			countries4.setFullName("Azerbaijan");//全称
+			countries4.setDescEn("Azerbaijan");//英文描述
+			countries4.setPhoneCode("994");//电话代码
+			ret.addCountries(countries4);
 			
 			ret.setRetCode("01");
 			ret.setRetMsg("success");
