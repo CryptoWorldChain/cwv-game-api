5a38d17377caffcbdb68786890dfb94ad9510a19
dumingyue <dumy@finchain60.com>
2018-07-16 17:37:14 +0800

    添加invoker父类

--- game/src/main/java/org/brewchain/cwv/game/chain/Invoker.java
+++ game/src/main/java/org/brewchain/cwv/game/chain/Invoker.java
@@ -1,5 +1,7 @@
 package org.brewchain.cwv.game.chain;
 
+import java.math.BigDecimal;
+
 import org.apache.felix.ipojo.annotations.Instantiate;
 import org.apache.felix.ipojo.annotations.Provides;
 import org.brewchain.cwv.auth.enums.ContractTypeEnum;
@@ -8,6 +10,7 @@ import org.brewchain.cwv.dbgens.game.entity.CWVGameContractAddress;
 import org.brewchain.cwv.dbgens.game.entity.CWVGameContractAddressExample;
 import org.brewchain.cwv.game.dao.Daos;
 import org.brewchain.cwv.game.helper.CommonHelper;
+import org.brewchain.wallet.service.Wallet.RespCreateTransaction;
 
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
@@ -37,5 +40,23 @@ public class Invoker {
 		
 		Object o  = dao.contractDao.selectOneByExample(example);
 		return o == null ? null : ((CWVGameContractAddress) o).getContractAddress() ;
+	}
+	
+	
+	RespCreateTransaction.Builder executeContract(String address, String type, String method, Object...objs){
+		RespCreateTransaction.Builder ret = RespCreateTransaction.newBuilder();
+		String data = wltHelper.excuteContract("1", "getFixedRange",objs);
+
+		String contractAddress = this.getContractAddress(ContractTypeEnum.RANDOM_CONTRACT.getName());
+		
+		if(contractAddress == null) {
+			ret.setRetCode(-1);
+			ret.setRetMsg("合约地址为空，暂不支持操作");
+			return ret;
+		}
+		
+		ret = wltHelper.excuteContract(new BigDecimal(0), address, contractAddress,data);
+		
+		return ret.setRetCode(1);
 	}
 }
